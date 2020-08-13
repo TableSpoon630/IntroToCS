c543a1928bc6551372b990897f6915df69af081d
Gaven Wright <gavenlwright@hotmail.com>
2020-08-13 11:39:56 -0700

    Added a comment about EYE

--- VariablesOperatorsExpressions/Program.cs
+++ VariablesOperatorsExpressions/Program.cs
@@ -11,6 +11,7 @@ namespace VariablesOperatorsExpressions
         static void Main(string[] args)
         {
             Console.WriteLine("I want to work at the Umbrella Corp.");
+            Console.WriteLine("EYE Divine Cybermancy is a sleeper hit."); // Find this on Steam
             Console.ReadLine();
         }
     }
